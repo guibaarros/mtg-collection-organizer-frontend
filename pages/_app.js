e32840a071b05c1d913b0f19ab24426657bf26f4
guibaarros <guilhermehr00@gmail.com>
2022-05-23 03:36:19 -0300

    feat: adding animation on login container

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles.css'
+import "../styles.css"
+import "../pagestyles/login.css"
+import "../components/button.css"
+import "../components/textfield/textfield.css"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function _app({ Component, pageProps }) {
